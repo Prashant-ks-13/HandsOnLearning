75f2a3bafed3725016d2a39c3cc2bfac771009c2
Prashant Kumar Singh <prashantsingh21000@gmail.com>
2025-10-17 18:31:58 +0530

    connect to database

---
+++ ecommerce/src/main/java/com/jpalearning/ecommerce/entity/Product.java
@@ -0,0 +1,85 @@
+package com.jpalearning.ecommerce.entity;
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+import java.math.BigDecimal;
+
+@Entity
+public class Product {
+
+    @Id
+    private long id;
+    private String name;
+    private String sku;
+    private String description;
+    private BigDecimal price;
+    private String imageurl;
+    private String created;
+    private String lastupdated;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSku() {
+        return sku;
+    }
+
+    public void setSku(String sku) {
+        this.sku = sku;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+
+    public String getImageurl() {
+        return imageurl;
+    }
+
+    public void setImageurl(String imageurl) {
+        this.imageurl = imageurl;
+    }
+
+    public String getCreated() {
+        return created;
+    }
+
+    public void setCreated(String created) {
+        this.created = created;
+    }
+
+    public String getLastupdated() {
+        return lastupdated;
+    }
+
+    public void setLastupdated(String lastupdated) {
+        this.lastupdated = lastupdated;
+    }
+}
