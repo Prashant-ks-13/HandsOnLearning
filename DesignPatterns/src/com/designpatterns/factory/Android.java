b15d1953369731a22ce0ca9370e30f1c85a6e21c
Prashant Kumar Singh <prashantsingh21000@gmail.com>
2025-10-27 23:49:38 +0530

    Add Singleton and Factory Design Pattern Learnings

---
+++ DesignPatterns/src/com/designpatterns/factory/Android.java
@@ -0,0 +1,8 @@
+package com.designpatterns.factory;
+
+public class Android implements OS {
+    @Override
+    public void specs() {
+        System.out.println("Android");
+    }
+}
---
+++ DesignPatterns/src/com/designpatterns/factory/OS.java
@@ -0,0 +1,5 @@
+package com.designpatterns.factory;
+
+public interface OS {
+    void specs();
+}
